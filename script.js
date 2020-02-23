22bb1244c87fce7521d024838305ce0bbe627697
jviccora <joe.viccora@libertasfunding.com>
2020-02-22 22:49:16 -0500

    password now generates working on prompts instead of set values

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const generatePassword = (
+    passwordLength = 8,
+    useUpperCase = true,
+    useNumbers = true,
+    useSpecialChars = true,
+) => {
+    const chars = 'abcdefghijklmnopqrstuvwxyz'
+    const numberChars = '0123456789'
+    const specialChars = '!"£$%^&*()'
+
+    const usableChars = chars +
+        (useUpperCase ? chars.toUpperCase() : '') +
+        (useNumbers ? numberChars : '') +
+        (useSpecialChars ? specialChars : '')
+
+    let generatedPassword = ''
+
+    for (i = 0; i <= passwordLength; i++) {
+        generatedPassword += usableChars[Math.floor(Math.random() * (usableChars.length))]
+    }
+
+    return generatedPassword
+}
